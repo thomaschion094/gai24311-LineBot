145c101d7614b37b5652079094aed01e5885f5a7
243-ThomasChiu <46102269+thomaschion094@users.noreply.github.com>
2024-09-12 01:01:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-import json
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+from flask import Flask
 app = Flask(__name__)
 
-# 讀取 config.json，取得 secret 和 token
-CONFIG = json.load(open("/home/24311/config.json", "r"))
-LINE_SECRET = CONFIG["line"]["line_secret"]
-LINE_TOKEN = CONFIG["line"]["line_token"]
-LINE_BOT = LineBotApi(LINE_TOKEN)
-HANDLER = WebhookHandler(LINE_SECRET)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    # X-Line-Signature: 數位簽章
-    signature = request.headers["X-Line-Signature"]
-    print(signature)
-    body = request.get_data(as_text=True)
-    print(body)
-    try:
-        HANDLER.handle(body, signature)
-    except InvalidSignatureError:
-        print("Check the channel secret/access token.")
-        abort(400)
-    return "OK"
+if __name__ == '__main__':
+    app.run()
