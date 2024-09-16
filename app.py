5205c8a21e537e494e94028f6b545d14781d3240
243-ThomasChiu <46102269+thomaschion094@users.noreply.github.com>
2024-09-16 15:31:15 +0800

    Update app.py
    edit api key

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import json
+import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 def linebot(request):
     try:
-        access_token = 'FcJ4pkfxpTb2w092vkxqxFPGmUkqzkEYITKCQo570s7m3W24FXKKGQwyeZoIMkk/ODd1pPjXrvy6GVwUzMCtEa35f7CqO33JM6TTlMDxI3hg7gaTFvvL8k/pgNWO/Yc5oBsnI8axYg9/WQxIV1tU8AdB04t89/1O/w1cDnyilFU='
-        secret = '0078b45ed2d1cd04c8da9fe6797a80d1'
+        access_token = os.environ.get('LINE_BOT_ACCESS_TOKEN')
+        secret = os.environ.get('LINE_BOT_SECRET')
         body = request.get_data(as_text=True)
         json_data = json.loads(body)
         line_bot_api = LineBotApi(access_token)
