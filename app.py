212ab1e9f142bb6288112bc653d7beb1352c89ee
243-ThomasChiu <46102269+thomaschion094@users.noreply.github.com>
2024-09-17 20:36:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
+from flask import Flask, request
+
+# 載入 json 標準函式庫，處理回傳的資料格式
 import json
-import os
+
+# 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-def linebot(request):
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
-        access_token = os.environ.get('LINE_BOT_ACCESS_TOKEN')
-        secret = os.environ.get('LINE_BOT_SECRET')
-        body = request.get_data(as_text=True)
-        json_data = json.loads(body)
-        line_bot_api = LineBotApi(access_token)
-        handler = WebhookHandler(secret)
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
-        msg = json_data['events'][0]['message']['text']
-        tk = json_data['events'][0]['replyToken']
-        line_bot_api.reply_message(tk,TextSendMessage(msg))
-        print(msg, tk)
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        access_token = 'FcJ4pkfxpTb2w092vkxqxFPGmUkqzkEYITKCQo570s7m3W24FXKKGQwyeZoIMkk/ODd1pPjXrvy6GVwUzMCtEa35f7CqO33JM6TTlMDxI3hg7gaTFvvL8k/pgNWO/Yc5oBsnI8axYg9/WQxIV1tU8AdB04t89/1O/w1cDnyilFU='
+        secret = '0078b45ed2d1cd04c8da9fe6797a80d1'
+        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
+        if type=='text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            print(msg)                                       # 印出內容
+            reply = msg
+        else:
+            reply = '你傳的不是文字呦～'
+        print(reply)
+        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
     except:
-        print(request.args)
-    return 'OK'
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+
+if __name__ == "__main__":
+    app.run()
